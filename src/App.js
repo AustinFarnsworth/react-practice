f2fa1f62091001e9b067c9e4853409c9f4d009db
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2021-03-12 17:03:48 -0700

    header and footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import "./App.css";
 import Hello from "./components/sayHello";
-import Header from './components/Header'
+import Header from './components/Header';
+import Footer from "./components/footer"
 
 
 function App() {
@@ -9,17 +10,21 @@ function App() {
   
 
   return (
+    
    <div className="app">
-      <Header />
-
-     <h1>My Favorite Sports</h1>
+     
+     <Header />
+     {/* <h1>My Favorite Sports</h1>
      <ul>
        <li>Basketball</li>
        <li>Soccer</li>
        <li>Water Polo</li>
-     </ul> 
+     </ul>  */}
     <Hello />
 
+    <Footer />
+    
+   
     
      
     </div>  
